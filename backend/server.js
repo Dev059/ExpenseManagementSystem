7d5bc70cd24330fa82be70016eca36d3f133bf40
Devesh Darshan Singh <sdevesh059@gmail.com>
2025-04-06 15:52:07 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,17 @@ app.use("/api/v1/users", require("./routes/userRoutes"));
 //transections routes
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
-//static files
-app.use(express.static(path.join(__dirname, "./client/build")));
+// //static files
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.get('/',(req,res)=>{
+  res.send('Hello')
+})
+
 
 //port
 const PORT = 8080 || process.env.PORT;
